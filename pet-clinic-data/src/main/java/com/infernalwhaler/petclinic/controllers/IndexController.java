cdf04998a4700a36914d4ba96043b50e77dfa8d8
Steven Deseure <deseure.s@gmail.com>
2021-10-26 14:19:27 +0200

    adding index page and indexController. closes #8

---
+++ pet-clinic-data/src/main/java/com/infernalwhaler/petclinic/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package com.infernalwhaler.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author sDeseure
+ * @project pet-clinic
+ * @date 26/10/2021
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index.html"})
+    public String index(){
+
+        return "index";
+    }
+}
