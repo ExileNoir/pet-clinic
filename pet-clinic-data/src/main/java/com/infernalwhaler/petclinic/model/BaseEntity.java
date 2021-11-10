7bdc0002c5823a288eb3d98950500523037ab852
Steven Deseure <deseure.s@gmail.com>
2021-11-10 13:33:39 +0100

    updated base entity with JPA annotations. closes #35

--- pet-clinic-data/src/main/java/com/infernalwhaler/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/infernalwhaler/petclinic/model/BaseEntity.java
@@ -1,15 +1,25 @@
 package com.infernalwhaler.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
+ * Simple JavaBean Model Object with an ID property.
+ * Used as a base class for objects needing this property.
+ *
  * @author sDeseure
  * @project pet-clinic
  * @date 22/10/2021
  */
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
--- pet-clinic-web/src/main/java/com/infernalwhaler/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/infernalwhaler/petclinic/controllers/IndexController.java
@@ -14,7 +14,6 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index.html"})
     public String index() {
-
         return "index";
     }
 
